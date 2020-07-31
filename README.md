d403c747015f99a76be8bb5503fa6c638bd029ac
AlphaBravoCompany <56687263+AlphaBravoCompany@users.noreply.github.com>
2020-07-31 16:20:46 -0400

    Feature/nginx proxy (#13)
    * add nginx proxy and update install script
    
    * update install script to create certs directory
    
    * update docker-compose file with kibana ports
    
    * fixing docker-compose errors
    
    * update variable replacement for install script
    
    * fix variables
    
    * fix variables
    
    * fix variables
    
    * update import file and install script sed replacement
    
    * correct syntax in setup for FQDN replacement
    
    * add timer to installer
    
    * update readme
    
    Co-authored-by: Mike 'MJ' Johnson <mikejohnsonit@gmail.com>

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # LINVIEW OSS for LINSTOR (Beta)
 
-## Version: 0.6-beta (released 06/11/20)
+## Version: 0.7-beta (released 07/24/20)
 
 ## This project is being heavily developed and there may be breaking changes.
 
@@ -9,6 +9,9 @@
 **LINSTOR** is Block Storage Management For Containers.
 
 With native integration to Kubernetes, LINSTOR makes building, running, and controlling block storage simple. LINSTOR is open-source software designed to manage block storage devices for large Linux server clusters. It’s used to provide persistent Linux block storage for Kubernetes, OpenStack, OpenNebula, and OpenShift environments. 
+
+## What is LINVIEW OSS?
+Linview OSS combines custom code with containerized Elastic, Kibana and Metricbeat to query the LINSTOR API and provide insight into your LINSTOR cluster. You do not need to provide Elastic or Kibana servers, it is all included in this deployment.
 
 ## Components
 * Docker
@@ -19,11 +22,13 @@ With native integration to Kubernetes, LINSTOR makes building, running, and cont
 Elastic, Metricbeat and Kibana are often referred to as EMK, so when you see references to EMK, that is what it is referring to.
 
 ## Requirements
-* Machine or VM with 4 CPU and 8GB of RAM
+* Machine or VM with 4 CPU/ 8GB of RAM / 100GB Disk
 * Linux
 * Docker CE Engine (https://docs.docker.com/engine/install/)
 * Docker Compose (https://docs.docker.com/compose/install/)
 * Target LINSTOR cluster*
+* OpenSSL
+* EPEL Repo (Centos Only)
 
 *If you need to configure a basic LINSTOR cluster on Ubuntu, you can follow instructions below:
 
@@ -35,19 +40,18 @@ We are actively developing installation scripts to streamline controller and sat
 
 ## Setting up LINVIEW OSS
 1. On the machine where LINVIEW OSS will be hosted, clone this repo.
-2. Change int the LINSTOR OSS directory `cd linstor-oss`
+2. Change int the LINSTOR OSS directory `cd linview-oss`
 3. Add execute to the setup script `chmod +x ./setup.sh`
-4. Run `./setup.sh` and enter the details for the Docker/Kibana host and the LINSTOR Controller host.
-5. Login to the interface at `http://dockerhostip:5601` with username and pass that you provided during install.
+4. Run `./setup.sh` and enter the details for the Docker/Kibana host and the LINSTOR Controller host. The install automatically creates a local cert based on the information provided to configure HTTPS access to the system.
+5. Login to the interface at `https://KIBANAFQDNORIP` (the FQDN you provided) with username `elastic` and password that you provided during install.
 
 ## Updating and Import Dashboards
-1. In the `import` directory, copy `LINSTORHOST-linview-kibana.ndjson file to your local machine.
+1. In the `import` directory on the Docker host, copy `LINSTORHOST-linview-kibana.ndjson` file to your local machine.
 2. In Kibana, navigate to `management`, `Kibana`, `Saved Objects` and the choose `Import` and select the modified import file from step 1.
 3. Now you can navigate to LINVIEW saved objects in the interface.
 
 ## Roadmap
 * Interface "Quick Start Guide"
-* Update on API elements collected in Elasticsearch
 * Add support to use Open Distro for Elastic
 * Updated dashboard elements for a more concise and informative layout
 * Instructions for alerting using Elastic xPack alerting (paid Elastic feature)
--- setup.sh
+++ setup.sh
@@ -1,22 +1,38 @@
 #!/bin/bash
 ####
 # This script does the following:
+# - Collects information from end user about the installation environment including IP addresses and FQDN
+# - Generates a temporary certificate with IP and DNS names for https access
 # - Updates the import.json file with proper values for the host and cluster where it will be installed.
-# - 
+# - Add correct values to configuration files from Elastic, Kibana and Metricbeat
+# - Start the Docker stack
+
+set -o errexit
+set -o nounset
 
 NOCOLOR='\033[0m'
 GREEN='\033[0;32m'
+BLUE='\033[0;34m'
 
 echo
-echo -e ${GREEN}Please provide the Docker Host / Kibana Host IP or FQDN and Port that Kibana Will be accessed on.${NOCOLOR}
+echo -e ${GREEN}Please provide the Docker Host system IP${NOCOLOR}
 echo
 
-read -p 'Protocol/Kibana (http or https): ' kibanaprotocol
-read -p 'Docker/Kibana Host IP or FQDN: ' kibanahost
-read -p 'Kibana Port: ' kibanaport
-read -p 'Kibana Username: ' kibanauser
+read -p 'Docker/Kibana Host IP or FQDN: ' dockerhostip
+
+echo
+echo -e ${GREEN}Please provide a wildcard domain name for internal cert generation.${NOCOLOR}
+echo
+
+read -p 'Wildcard domain for internal cert generation (eg. *.example.com): ' wildcarddomain
+
+echo
+echo -e ${GREEN}Please provide the Docker Host / Kibana Host FQDN that Kibana Will be accessed on.${NOCOLOR}
+echo
+
+read -p 'Kibana FQDN (eg. kibana.linview.io): ' kibanafqdn
+read -p 'Kibana Port (If you have not updated this in docker-compose, enter 5601): ' kibanaport
 read -s -p 'Kibana Password: ' kibanapass
-
 
 echo
 echo -e ${GREEN}Please provide the LINSTOR Controller IP or FQDN and Port that the LINSTOR API will be accessed on.${NOCOLOR}
@@ -24,30 +40,82 @@ echo
 
 read -p 'LINSTOR Protocol (http or https): ' linstorprotocol
 read -p 'LINSTOR Controller IP or FQDN: ' linstorhost
-read -p 'LINSTOR Port: ' linstorport
+read -p 'LINSTOR Port (Enter 3370 if you did not modify the default port): ' linstorport
+
+# Generate internal x509 certificates
+mkdir ./emk/certs/
+openssl req -x509 -nodes -days 365 -subj "/C=US/ST=DC/O=LINVIEW, Inc./CN=$wildcarddomain" -addext "subjectAltName=IP:$dockerhostip,DNS:$kibanafqdn" -newkey rsa:2048 -keyout ./emk/certs/shared.key -out ./emk/certs/shared.crt;
 
 # Copy example Kibana import variables to new file with Linstor Controller name
 cp ./import/linview_kibana_example.ndjson ./import/$linstorhost-linview-kibana.ndjson
 
 # Replace variables in Kibana import file with proper provided values
-sed -i "s/\KIBANAPROTOCOL/$kibanaprotocol/g" ./import/$linstorhost-linview-kibana.ndjson
-sed -i "s/\KIBANAHOST/$kibanahost/g" ./import/$linstorhost-linview-kibana.ndjson
-sed -i "s/\KIBANAPORT/$kibanaport/g" ./import/$linstorhost-linview-kibana.ndjson
+sed -i "s/\DOCKER_HOST_FQDN/$kibanafqdn/g" ./import/$linstorhost-linview-kibana.ndjson
 
 # Update the Kibana config with the user provided username and password
-cp ./emk/kibana/config/kibana.yml ./emk/kibana/config/kibana.yml.orig
-sed -i "s/\KIBANAUSER/$kibanauser/g" ./emk/kibana/config/kibana.yml
+cp ./emk/kibana/config/kibana.yml ./emk/kibana/config/kibana-$(date +"%Y%m%dT%H%M").yml.orig
 sed -i "s/\KIBANAPASS/$kibanapass/g" ./emk/kibana/config/kibana.yml
 
 sed -i "s/\LINSTORPROTOCOL/$linstorprotocol/g" ./import/$linstorhost-linview-kibana.ndjson
-sed -i "s/\LINSTORHOST/$linstorhost/g" ./import/$linstorhost-linview-kibana.ndjson
+sed -i "s/\LINSTOR_CONTROLLER_FQDN/$linstorhost/g" ./import/$linstorhost-linview-kibana.ndjson
 sed -i "s/\LINSTORPORT/$linstorport/g" ./import/$linstorhost-linview-kibana.ndjson
 
 # Replace LINSTOR host in .env for Kibana setup
-cp ./emk/.env ./emk/.env.orig
+cp ./emk/.env ./emk/.env--$(date +"%Y%m%dT%H%M").orig
 sed -i "s/\LINSTORHOST/$linstorhost/g" ./emk/.env
 sed -i "s/\LINSTORPORT/$linstorport/g" ./emk/.env
+
+#Update Elastic/Kibana password for Elastic
+sed -i "s/\ELASTICPW/$kibanapass/g" ./emk/.env
+
+#Update Kibana FQDN for NGINX
+sed -i "s/\KIBANAFQDN/$kibanafqdn/g" ./emk/.env
+
+#Update Elastic/Kibana password for Metricbeat
+sed -i "s/\KIBANAPASS/$kibanapass/g" ./emk/metricbeat/config/metricbeat.yml
 
 # Run docker containers
 cd ./emk
 docker-compose up -d
+
+echo
+echo -e ${GREEN}Please wait 5 minutes while Elastic and Kibana are configured.${NOCOLOR}
+echo
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+RESET='\033[0m'
+hour=0
+min=5
+sec=0
+tput civis
+echo -ne "${RED}"
+        while [ $hour -ge 0 ]; do
+                 while [ $min -ge 0 ]; do
+                         while [ $sec -ge 0 ]; do
+                                 if [ "$hour" -eq "0" ] && [ "$min" -eq "0" ]; then
+                                         echo -ne "${YELLOW}"
+                                 fi
+                                 if [ "$hour" -eq "0" ] && [ "$min" -eq "0" ] && [ "$sec" -le "10" ]; then
+                                         echo -ne "${GREEN}"
+                                 fi
+                                 echo -ne "$(printf "%02d" $hour h):$(printf "%02d" $min m):$(printf "%02d" $sec s)\033[0K\r"
+                                 let "sec=sec-1"
+                                 sleep 1
+                         done
+                         sec=59
+                         let "min=min-1"
+                 done
+                 min=59
+                 let "hour=hour-1"
+         done
+echo -e "${RESET}"
+tput cnorm
+
+echo
+echo -e ${GREEN}Deployment is complete.
+echo -e Please visit: https://$kibanafqdn ${NOCOLOR}
+echo -e User: ${BLUE}elastic${NOCOLOR}
+echo -e Pass: ${BLUE}Password you provided for Kibana above${NOCOLOR}
+echo
